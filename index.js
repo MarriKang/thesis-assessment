8fba5c6de2738f65038f0ae75fa14336e857e4cb
Marri Kang <kangmarri@gmail.com>
2021-12-29 11:00:02 -0500

    Add transitions and optimize background image

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function openMenu() {
 function closeMenu() {
     document.getElementById('hamburgerMenu').style.width = "0";
 }
+
+const eyeIcon = document.getElementById('eye-icon');
+
+eyeIcon.addEventListener("click", () => {
+    const password = document.getElementById('pw-input');
+    const icon = document.getElementById('eye-icon');
+
+    if (password.getAttribute('type') === 'password') {
+        password.setAttribute('type', 'text');
+    } else {
+        password.setAttribute('type', 'password');
+    }
+
+    icon.classList.toggle('fa-eye');
+    icon.classList.toggle('fa-eye-slash');
+})
